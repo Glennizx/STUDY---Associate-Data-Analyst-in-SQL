dda5b8959960fa57a1d20b4f1acde22009649331
Gelade <glennezekielmiral@gmail.com>
2026-04-19 01:28:24 +0000

    commit

--- Course 6: Functions for Manipulating Data in PostGreSQL.sql
+++ Course 6: Functions for Manipulating Data in PostGreSQL.sql
@@ -119,3 +119,123 @@ FROM customer
 SELECT CONCAT(first_name, ' ', last_name,  ' <', email, '>') AS full_email 
 FROM customer
 
+/* Exercise 14: 
+Convert the film category name to uppercase.
+Convert the first letter of each word in the film's title to upper case.
+Concatenate the converted category name and film title separated by a colon.
+Convert the description column to lowercase.
+*/
+SELECT 
+  UPPER(c.name)  || ': ' || INITCAP(f.title) AS film_category, 
+  lower(description) AS description
+FROM 
+  film AS f 
+  INNER JOIN film_category AS fc 
+  	ON f.film_id = fc.film_id 
+  INNER JOIN category AS c 
+  	ON fc.category_id = c.category_id;
+
+
+-- Exercise 15:  Replace all whitespace with an underscore.
+SELECT 
+  REPLACE(title, ' ', '_') AS title
+FROM film; 
+
+/* Exercise 16:
+Select the title and description columns from the film table.
+Find the number of characters in the description column with the alias desc_len
+*/
+SELECT 
+  title,
+  description,
+  CHAR_LENGTH(description) AS desc_len
+FROM film;
+
+-- Exercise 17: Select the first 50 characters of the description column with the alias short_desc
+SELECT 
+  LEFT(description, 50) AS short_desc
+FROM 
+  film AS f; 
+
+/* Exercise 17:
+Extract only the street address without the street number from the address column.
+Use functions to determine the starting and ending position parameters.
+*/
+SELECT 
+  SUBSTRING(address FROM POSITION(' ' IN address)+1 FOR LENGTH(address))
+FROM 
+  address;
+
+  /* Exercise 18:
+Extract the characters to the left of the @ of the 
+email column in the customer table and alias it as username.
+Now use SUBSTRING to extract the characters after the @ 
+of the email column and alias the new derived field as domain.
+*/
+SELECT
+  LEFT(email, POSITION('@' IN email)-1) AS username,
+  SUBSTRING(email FROM POSITION('@' IN email)+1 FOR LENGTH(email)) AS domain
+FROM customer;
+
+/* Exercise 19:
+Add a single space to the end or right of the first_name column using a padding function.
+Use the || operator to concatenate the padded first_name to the last_name column.
+*/
+SELECT 
+	RPAD(first_name, LENGTH(first_name)+1) || last_name AS full_name
+FROM customer;
+
+/* Exercise 20:
+Now add a single space to the left or beginning of the last_name column using a different padding function than the first step.
+Use the || operator to concatenate the first_name column to the padded last_name.
+*/
+SELECT 
+	first_name || LPAD(last_name, LENGTH(last_name)+1) AS full_name
+FROM customer; 
+
+/* Exercise 21:
+Add a single space to the right or end of the first_name column.
+Add the characters < to the right or end of last_name column.
+Finally, add the characters > to the right or end of the email column.+
+*/
+SELECT 
+	RPAD(first_name, LENGTH(first_name)+1) 
+    || RPAD(last_name, LENGTH(last_name)+2, ' <') 
+    || RPAD(email, LENGTH(email)+1, '>') AS full_email
+FROM customer; 
+
+/* Exercise 22:
+Convert the film category name to uppercase and use the CONCAT() 
+concatenate it with the title.
+Truncate the description to the first 50 characters and 
+make sure there is no leading or trailing whitespace after truncating.
+*/
+SELECT 
+  CONCAT(UPPER(c.name), ': ', f.title) AS film_category, 
+  TRIM(LEFT(description, 50)) AS film_desc
+FROM 
+  film AS f 
+  INNER JOIN film_category AS fc 
+  	ON f.film_id = fc.film_id 
+  INNER JOIN category AS c 
+  	ON fc.category_id = c.category_id;
+
+  /* Exercise 23:
+Get the first 50 characters of the description column
+Determine the position of the last whitespace character 
+of the truncated description column and subtract it from 
+the number 50 as the second parameter in the first function above.
+*/
+SELECT 
+  UPPER(c.name) || ': ' || f.title AS film_category, 
+  LEFT(description, 50 - 
+    POSITION(
+      ' ' IN REVERSE(LEFT(description, 50))
+    )
+  ) 
+FROM 
+  film AS f 
+  INNER JOIN film_category AS fc 
+  	ON f.film_id = fc.film_id 
+  INNER JOIN category AS c 
+  	ON fc.category_id = c.category_id;
