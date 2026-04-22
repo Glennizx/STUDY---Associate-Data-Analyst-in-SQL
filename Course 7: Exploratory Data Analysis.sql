e453379bf21d94355864a0f5b0a6605e3eb3ebe9
Gelade <glennezekielmiral@gmail.com>
2026-04-22 02:56:33 +0000

    commit

--- Course 7: Exploratory Data Analysis.sql
+++ Course 7: Exploratory Data Analysis.sql
@@ -32,7 +32,7 @@ SELECT type, count(*) AS count
 
  ORDER BY count DESC;
 
- /* $5.
+ /* #5.
 Join the tag_company, company, and tag_type tables, keeping only mutually occurring records.
 Select company.name, tag_type.tag, and tag_type.type for tags with the most common type
  from the previous step.
@@ -45,3 +45,86 @@ SELECT name, tag_type.tag, tag_type.type
        INNER JOIN tag_type
        ON tag_company.tag = tag_type.tag
   WHERE type='cloud';
+
+  /* #6.
+Use coalesce() to select the first non-NULL value from industry, sector, or 'Unknown' as a fallback value.
+Alias the result of the call to coalesce() as industry2.
+Count the number of rows with each industry2 value.
+Find the most common value of industry2.
+
+  */
+SELECT COALESCE(industry, sector, 'Unknown') AS industry2,
+       
+       COUNT(*) 
+  FROM fortune500
+ GROUP BY industry2
+ ORDER BY count DESC
+ LIMIT 1;
+
+ /* #7.
+Select profits_change and profits_change cast as integer from fortune500.
+Look at how the values were converted.
+ */ 
+SELECT profits_change, 
+       CAST(profits_change AS integer) AS profits_change_int
+  FROM fortune500;
+
+/* #8.
+Select profits_change and profits_change cast as integer from fortune500.
+Look at how the values were converted.
+*/ 
+SELECT 10/3, 
+       10::numeric/3;
+
+/* #9.
+Now cast numbers that appear as text as numeric.
+Note: 1e3 is scientific notation.
+*/ 
+SELECT '3.2'::numeric,
+       '-123'::numeric,
+       '1e3'::numeric,
+       '1e-3'::numeric,
+       '02314'::numeric,
+       '0002'::numeric
+
+
+/* #10.
+Use GROUP BY and count() to examine the values of revenues_change.
+Order the results by revenues_change to see the distribution.
+*/
+SELECT revenues_change, count(*) 
+  FROM fortune500
+ GROUP BY revenues_change 
+ ORDER BY revenues_change;
+
+ /* #11.
+Repeat step 1, but this time, cast revenues_change as an integer 
+to reduce the number of different values.
+*/
+SELECT revenues_change::integer, COUNT(*)
+  FROM fortune500
+ GROUP BY revenues_change::integer
+ ORDER BY revenues_change;
+
+
+  /* #12.
+How many of the Fortune 500 companies had revenues increase in 2017 compared to 2016?
+To find out, count the rows of fortune500 where revenues_change indicates an increase.
+*/
+-- Count rows 
+SELECT COUNT(*)
+  FROM fortune500
+ -- Where...
+ WHERE revenues_change > 0;
+
+/* #13.
+Compute revenue per employee by dividing revenues by employees; casting is used here to produce a numeric result.
+Take the average of revenue per employee with avg(); alias this as avg_rev_employee.
+Group by sector.
+Order by the average revenue per employee.
+*/
+SELECT sector, 
+       AVG(revenues/employees::numeric) AS avg_rev_employee
+  FROM fortune500
+ GROUP BY sector
+ ORDER BY avg_rev_employee;
